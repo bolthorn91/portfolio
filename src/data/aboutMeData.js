08fc1958744f93dea1edb6964ebffb81757e297f
bolthorn91 <bolthorn91@gmail.com>
2023-07-12 19:54:34 +0100

    Add new career descroptions to my bio

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -2,7 +2,7 @@ export const aboutMeData = [
 	{
 		id: 1,
 		title: 'Web engineering',
-		bio: `As a senior web development engineer, I have worked on web application development using various technologies. I have used React, Typescript, and Redux to build web applications, managing application state and conducting unit, integration, and end-to-end testing using Jest and Cypress. I have employed methodologies like BEM for CSS development and implemented responsive designs using SASS. \n\n
+		bio: `As a senior web development engineer, I have worked on web application development using various technologies. I have used React, Typescript, and Redux to build web applications, managing application state and conducting unit, integration, and end-to-end testing using Jest and Cypress. I have employed methodologies like BEM for CSS development and implemented responsive designs using SASS.
 		
 		In summary, I have experience web development using technologies like React, Typescript, Redux, Angular, and Vue. I have applied comprehensive testing in both areas and have a strong understanding of development best practices in each domain, backend and frontend.
 		
@@ -16,23 +16,44 @@ export const aboutMeData = [
 	},
 	{
 		id: 3,
-		title: 'CTO & Tech Leading',
-		bio: `As a senior web development engineer, I have worked on web application development using various technologies. I have used React, Typescript, and Redux to build web applications, managing application state and conducting unit, integration, and end-to-end testing using Jest and Cypress. I have employed methodologies like BEM for CSS development and implemented responsive designs using SASS. Additionally, I have experience in web development using Angular and Vue.
+		title: 'CTO',
+		bio: `I have been CTO on Squaads company and my commitments has been: 
 		
-		In summary, I have experience in both mobile development and web development. I have worked on mobile application development projects for Android and iOS using frameworks like Ionic and Firebase, as well as web application development using technologies like React, Typescript, Redux, Angular, and Vue. I have applied comprehensive testing in both areas and have a strong understanding of development best practices in each domain.`,
+		Setting the technology strategy and vision aligned with business goals, leading and managing technology teams to drive innovation and collaboration, overseeing product development and leveraging emerging technologies, designing and implementing scalable and secure technology infrastructure.
+
+		Establishing strategic partnerships and managing vendor relationships, ensuring cybersecurity and managing technology-related risks, communicating effectively with stakeholders and presenting technology strategies.
+		
+		Guiding the technology direction, leading teams, driving innovation, managing infrastructure, and ensuring effective communication within the organization.`,
 	},
 	{
 		id: 4,
-		title: 'Teaching & Mentoring',
-		bio: `As a senior web development engineer, I have worked on web application development using various technologies. I have used React, Typescript, and Redux to build web applications, managing application state and conducting unit, integration, and end-to-end testing using Jest and Cypress. I have employed methodologies like BEM for CSS development and implemented responsive designs using SASS. Additionally, I have experience in web development using Angular and Vue.
+		title: 'Tech Leading',
+		bio: `I have worked as a Tech Lead on multiple companies and projects and my commitments has been:
 		
-		In summary, I have experience in both mobile development and web development. I have worked on mobile application development projects for Android and iOS using frameworks like Ionic and Firebase, as well as web application development using technologies like React, Typescript, Redux, Angular, and Vue. I have applied comprehensive testing in both areas and have a strong understanding of development best practices in each domain.`,
+		Providing technical leadership and guidance to development teams, collaborating with stakeholders to define project goals and requirements, architecting and designing scalable and efficient technical solutions, leading the implementation and coding of complex features.
+		
+		Mentoring and coaching team members to enhance their skills and knowledge, conducting code reviews and ensuring code quality and best practices, managing project timelines, priorities, and resource allocation, troubleshooting technical issues and providing timely resolutions.
+
+		I have been responsible for leading and coordinating development efforts, ensuring technical excellence, fostering teamwork and collaboration, and delivering high-quality solutions in alignment with project goals and requirements.`,
 	},
 	{
 		id: 5,
-		title: 'Videogames engineering',
-		bio: `As a senior web development engineer, I have worked on web application development using various technologies. I have used React, Typescript, and Redux to build web applications, managing application state and conducting unit, integration, and end-to-end testing using Jest and Cypress. I have employed methodologies like BEM for CSS development and implemented responsive designs using SASS. Additionally, I have experience in web development using Angular and Vue.
+		title: 'Mentoring',
+		bio: `During my career, he mentored multiple students, professionals, and companies in different areas of the software industry. 
 		
-		In summary, I have experience in both mobile development and web development. I have worked on mobile application development projects for Android and iOS using frameworks like Ionic and Firebase, as well as web application development using technologies like React, Typescript, Redux, Angular, and Vue. I have applied comprehensive testing in both areas and have a strong understanding of development best practices in each domain.`,
+		I have worked with international companies in tailor-made mentoring. I have benn part of mentoring in bootcamps and courses in the sector, collaborating with American companies such as Springboard and universities or collaborating with educational entities and organizations such as the ULPG (University of Las Palmas de Gran Canaria) or the EOI (School of Industrial Organization). 
+		
+		I also provide personal mentoring to professionals in the sector.`
 	},
+	{
+		id: 6,
+		title: 'Teaching',
+		bio: `I have taught various courses related to the industry. I have collaborated with the EOI in courses and bootcamps such as Fullstack web development or Frontend development courses.
+
+		I have also been director and teacher of the testing course for several years in the same organization.
+		
+		I have also created courses and masterclasses of some technologies and languages ​​for both companies and individuals.`
+	},
+	
+	
 ];
