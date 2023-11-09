6551ab30db5c147278d675471e33fa0e173722d1
bolthorn91 <bolthorn91@gmail.com>
2023-11-09 10:48:06 +0000

    Add styles to personalinfo component for cv selector page

--- src/components/cv/CVPersonalInfo.jsx
+++ src/components/cv/CVPersonalInfo.jsx
@@ -1,14 +1,15 @@
 export const CVPersonalInfoComponent = ({
     profileImage,
     name,
-    position
+    position,
+    isSelector
 }) => {
     return (
-        <section className="flex align-center justify-center w-full text-center py-5 sm:py-10 mt-5 sm:mt-10">
-        <div className="w-full sm:w-1/4 mb-7 sm:mb-0">
+        <section className={`${!isSelector ? 'text-center py-5' : 'p-5'} flex items-center justify-center w-full sm:py-10 mt-5 sm:mt-10`}>
+        <div className={`${isSelector ? 'mr-4' : ''} w-full sm:w-1/4 mb-7 sm:mb-0`}>
             <img src={profileImage} className="rounded-lg w-96 user-none" alt="" />
         </div>
-        <div className="w-full text-center py-5 sm:py-10 mt-5 sm:mt-10">
+        <div className={`${!isSelector ? 'w-full text-center' : ''} py-5 sm:py-10 mt-5 sm:mt-10`}>
             <p className="mb-2 text-xl">{name}</p>
             <p className="text-">{position}</p>
         </div>
